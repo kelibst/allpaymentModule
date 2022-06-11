da88b5e1083d3d71162b1b1d9a546d04dff1a161
Keli Booster <kbooster17@gmail.com>
2022-06-11 05:32:40 +0000

    initial testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+function randomNoGenerator(min, max) {
+  if (typeof max !== "number" && typeof min !== "number") {
+    min = 0;
+    max = 1;
+  }
+  return Math.random() * (max - min) + min;
+}
+module.exports = randomNoGenerator;
