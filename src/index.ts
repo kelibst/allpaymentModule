e1302b8ac643ef5b37f501e7447f491d777ca563
Keli Booster <kbooster17@gmail.com>
2022-06-11 17:05:20 +0000

    add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function randomNoGenerator(min, max) {
+function randomNoGenerator(min: number, max: number) {
   if (typeof max !== "number" && typeof min !== "number") {
     min = 0;
     max = 1;
@@ -6,3 +6,5 @@ function randomNoGenerator(min, max) {
   return Math.random() * (max - min) + min;
 }
 module.exports = randomNoGenerator;
+
+console.log(randomNoGenerator(5, 8));
